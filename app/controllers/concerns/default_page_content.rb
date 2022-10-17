7971ccc27aa15c381827c984c9599935936daaa9
stuart wilson <stuartwilson94@gmail.com>
2022-10-17 14:18:37 -0500

    Integrated favicons on all layouts

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "Portfolio | My Portfolio Website"
     @seo_keywords = "Stuart Wilson Portfolio"
   end
 end
