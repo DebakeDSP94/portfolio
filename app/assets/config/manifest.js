9892c1f5992389aa9d0448ce4738ea0c6a6cc2be
stuart wilson <stuartwilson94@gmail.com>
2022-10-23 17:30:19 -0500

    updated manifest js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link blogs.scss
-//= link portfolios.scss
+//= link blogs.css
+//= link portfolios.css
 //= link video_bg_small.mp4
 //= link homebg.png
 //= link actiontext.css
