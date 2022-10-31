ba045c4f76a5cf3cd53c879fed3edcebce0869aa
stuart wilson <stuartwilson94@gmail.com>
2022-10-31 15:17:02 -0500

    fixed error on empty blog submission and flash message. Remove unnecessary if from trix_mark

--- app/javascript/controllers/trix_mark_controller.js
+++ app/javascript/controllers/trix_mark_controller.js
@@ -16,31 +16,29 @@ export default class extends Controller {
 		const trixElements = document.querySelectorAll('.trix-content');
 		trixElements.forEach((trixElement) => {
 			const preElements = trixElement.querySelectorAll('pre');
-			if (trixElement.length !== 0) {
-				preElements.forEach((preElement) => {
-					if (preElement.length !== 0) {
-						const regex = /(?!lang\-\\w\*)lang-\w*\W*/gm;
-						const codeElement = document.createElement('code');
-						if (preElement.childNodes.length > 1) {
-							console.error(
-								'<pre> element contained nested inline elements (probably styling) and could not be processed. Please remove them and try again.'
-							);
-						}
-						let preElementTextNode = preElement.removeChild(
-							preElement.firstChild
+			preElements.forEach((preElement) => {
+				if (preElement.length !== 0) {
+					const regex = /(?!lang\-\\w\*)lang-\w*\W*/gm;
+					const codeElement = document.createElement('code');
+					if (preElement.childNodes.length > 1) {
+						console.error(
+							'<pre> element contained nested inline elements (probably styling) and could not be processed. Please remove them and try again.'
 						);
-						let language = preElementTextNode.textContent.match(regex);
-						if (language) {
-							language = language[0].toString().trim();
-							preElementTextNode.textContent =
-								preElementTextNode.textContent.replace(language, '');
-							codeElement.classList.add(language, 'line-numbers');
-						}
-						codeElement.append(preElementTextNode);
-						preElement.append(codeElement);
 					}
-				});
-			}
+					let preElementTextNode = preElement.removeChild(
+						preElement.firstChild
+					);
+					let language = preElementTextNode.textContent.match(regex);
+					if (language) {
+						language = language[0].toString().trim();
+						preElementTextNode.textContent =
+							preElementTextNode.textContent.replace(language, '');
+						codeElement.classList.add(language, 'line-numbers');
+					}
+					codeElement.append(preElementTextNode);
+					preElement.append(codeElement);
+				}
+			});
 		});
 	};
 }
